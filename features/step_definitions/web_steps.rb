509aaca420daad944e12b21e3f3da40f2f200ddf
pmyyyy <promp@LAPTOP-69L9RNF3>
2023-09-17 17:33:59 +0000

    modify web_steps.rb

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -178,7 +178,6 @@ end
 
 Then /^(?:|I )should be on (.+)$/ do |page_name|
   current_path = URI.parse(current_url).path
-  assert_equal path_to(page_name), current_path
   if current_path.respond_to? :should
     current_path.should == path_to(page_name)
   else
