1bf50784d852bb23af376139126714c9e0e1b039
pmyyyy <promp@LAPTOP-69L9RNF3>
2023-09-17 15:45:24 +0000

    Revert "Revert "update movie_step.rb""
    This reverts commit f70880ccda644915328c12ec0a4321b17ab128ed.

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,9 +4,8 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
-    Movie.create(movie)
   end
-  #pending "Fill in this step in movie_steps.rb"
+  pending "Fill in this step in movie_steps.rb"
 end
 
 Then /(.*) seed movies should exist/ do | n_seeds |
@@ -19,63 +18,29 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.body is the entire content of the page as a string.
-  #pending "Fill in this step in movie_steps.rb"
-
-  begin
-    DateTime.parse e1
-    DateTime.parse e2
-
-    within(:xpath, %(//table[@id="movies"]/tbody)) do
-      release_dates = page.all(:xpath, "//td[3]").to_a.map do 
-        |el| DateTime.parse(el.text)
-      end.compact
-      
-      expect(release_dates.index(e1) < release_dates.index(e2)).to be true
-    end
-  rescue ArgumentError
-    expect(page.body.index(e1) < page.body.index(e2)).to be true
-  end
+  pending "Fill in this step in movie_steps.rb"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /^I press "(.*)" button/ do |button|
-  click_button button
-end
-
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
-  #pending "Fill in this step in movie_steps.rb"
-  ratings = rating_list.split(', ')
-  ratings.each do |rating|
-    #puts "rating"
-    #puts rating
-    uncheck ? uncheck("ratings[#{rating}]") : (check("ratings[#{rating}]"))
-  end
+  pending "Fill in this step in movie_steps.rb"
 end
 
 # Part 2, Step 3
-Then /^I should (not )?see the following movies: (.*)$/ do |present, movies_list|
+Then /^I should (not )?see the following movies: (.*)$/ do |no, movie_list|
   # Take a look at web_steps.rb Then /^(?:|I )should see "([^"]*)"$/
-  #pending "Fill in this step in movie_steps.rb"
-  movies = movies_list.split(', ')
-  movies.each do |movie|
-    if present.nil?
-      expect(page).to have_content(movie)
-    else
-      expect(page).not_to have_content(movie)
-    end
-  end
+  pending "Fill in this step in movie_steps.rb"
 end
 
 Then /I should see all the movies/ do
   # Make sure that all the movies in the app are visible in the table
-  #pending "Fill in this step in movie_steps.rb"
-  expect(page).to have_xpath(".//tr[not(ancestor::thead)]", :count => Movie.count)
+  pending "Fill in this step in movie_steps.rb"
 end
 
 ### Utility Steps Just for this assignment.
@@ -98,14 +63,4 @@ Then /complete the rest of of this scenario/ do
   # You should leave this block inside movie_steps, but replace
   # the line in your scenarios with the appropriate steps.
   fail "Remove this step from your .feature files"
-end
-
-Then /Then I should be on the Create New Movie page/ do |page_name|
-  current_path = URI.parse(current_url).path
-  #assert_equal path_to(page_name), current_path
-  if current_path.respond_to? :should
-    current_path.should == path_to(page_name)
-  else
-    assert_equal path_to(page_name), current_path
-  end
 end
