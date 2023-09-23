bb0ed8257e159105b541b40b5998072cce514ca8
pmyyyy <promp@LAPTOP-69L9RNF3>
2023-09-23 14:11:33 +0000

    modify homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,17 @@ Rottenpotatoes::Application.routes.draw do
   #get 'pages/home'
   resources :movies
   # map '/' to be a redirect to '/movies'
-  root to: 'movies#index'
+  #root to: 'movies#index'
   #root :to => redirect('/movies')
   #root :to => redirect('/users/sign_in')
   #root :to => redirect(new_user_session_path)
+  root 'pages#home'
   
   devise_for :users, controllers: {
     registrations:'users/registrations',
     sessions: 'users/sessions',
     omniauth_callbacks: 'users/omniauth_callbacks',
   }
+
+  #root 'devise/sessions#new'
 end
