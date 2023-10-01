96c49c05a43eb0b3124dc8c085dc8fc428d71221
pmyyyy <promp@LAPTOP-69L9RNF3>
2023-10-01 07:39:47 +0000

    finished single sign on

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,25 @@
 Rottenpotatoes::Application.routes.draw do
   #get 'pages/home'
-  resources :movies
+  # resources :movies
+
+  resources :movies do
+    resources :reviews
+  end
+
+# config/routes.rb
+
+  # # เพิ่มส่วนนี้เพื่อสร้างเส้นทางสำหรับการสร้างรีวิวใหม่
+  # resources :reviews, only: [:new, :create]
+  
+  # # เพิ่มส่วนนี้หากคุณยังมีเส้นทางสำหรับหน้าหลักของหนัง หรืออื่นๆ ที่คุณต้องการ
+  # resources :movies
+
   # map '/' to be a redirect to '/movies'
-  #root to: 'movies#index'
+  root to: 'movies#index'
   #root :to => redirect('/movies')
   #root :to => redirect('/users/sign_in')
   #root :to => redirect(new_user_session_path)
-  root 'pages#home'
+  #root 'pages#home'
   
   devise_for :users, controllers: {
     registrations:'users/registrations',
